a6b9059497b8a5ab3ae065607df59e8b3c4a1ff9
chotnho <97233253+chotnho@users.noreply.github.com>
2022-04-10 15:11:47 +0700

    1

---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class SingleRoom extends
+    Component {
+  render() {
+      return (
+        
+          <div>
+              this is singleRoom page
+              
+      </div>
+    )
+  }
+}
